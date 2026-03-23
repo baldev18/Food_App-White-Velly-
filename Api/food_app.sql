b886474af4964527ce247d661e34793f1c537e41
BARAIYA BALDEVSINH B. <baldevbaraiya18@gmail.com>
2026-03-23 14:29:36 +0530

    Add files via upload

---
+++ Api/food_app.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 23, 2026 at 09:54 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `food_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `baldev_users`
+--
+
+CREATE TABLE `baldev_users` (
+  `id` int(30) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `baldev_users`
+--
+
+INSERT INTO `baldev_users` (`id`, `name`, `email`, `pass`) VALUES
+(1, 'asdadad', 'b@gmail.com', '123456'),
+(6, 'badlefhbv', 'b@gmail.com', '123456'),
+(7, 'baldevfgnfg', 'b@gmail.com', '123456'),
+(8, 'hellooo', 'b@gmail.com', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `baldev_users`
+--
+ALTER TABLE `baldev_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `baldev_users`
+--
+ALTER TABLE `baldev_users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
